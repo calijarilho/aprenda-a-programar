05431a5ca7973f8115e39a7d16a689edb2a7741c
Cali <calijarilho@gmail.com>
2022-08-28 21:37:18 -0300

    tabuada

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 9
+let n = 9
+let i = 1
+
+while (i <= n) {
+    let resultado = multiplicador * i
+    let texto = `${multiplicador} x ${i} = ${resultado}`
+    console.log(texto)
+    i++
+}
